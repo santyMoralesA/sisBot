1c07d0dde94110b9a38569634849ad672af82ae3
Santiago Morales <smorales@tensolite.net>
2023-05-02 16:37:57 -0300

    cambios en la interfaz, se agregó logo

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const dfMessenger = document.querySelector('df-messenger');
-dfMessenger.addEventListener('event-type', function (event) {
-  // Handle event
-  
-});
+// const dfMessenger = document.querySelector('df-messenger');
+// dfMessenger.addEventListener('event-type', function (event) {
+//   // Handle event  
+// });
+
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + "/public/"));
+app.listen('3000', function() {
+   console.log("Servidor Web iniciado escuchando en puerto 3000. http://localhost:3000/");
+})
