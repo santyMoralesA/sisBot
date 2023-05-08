629a7cb0e9e73a723725262e74cf41adc144b534
Santiago Morales <smorales@tensolite.net>
2023-05-08 09:27:22 -0300

    modificaciones en el chat

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-// const dfMessenger = document.querySelector('df-messenger');
-// dfMessenger.addEventListener('event-type', function (event) {
-//   // Handle event  
-// });
-
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + "/public/"));
 app.listen('3000', function() {
    console.log("Servidor Web iniciado escuchando en puerto 3000. http://localhost:3000/");
 })
+
+const dfMessenger = document.querySelector('df-messenger');
+dfMessenger.addEventListener('event-type', function (event) {
+  // Handle event
+  
+});
+
