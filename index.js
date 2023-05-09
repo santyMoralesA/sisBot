2030adf4a4f06c597c8f159ffcd48c285bf53483
Santiago Morales <aplicaciones2.sistemas@tensolite.com>
2023-05-09 16:30:54 -0300

    merge a main

--- index.js
+++
@@ -1,5 +0,0 @@
-const dfMessenger = document.querySelector('df-messenger');
-dfMessenger.addEventListener('event-type', function (event) {
-  // Handle event
-  
-});
---
+++ public/chatbot.js
@@ -0,0 +1,40 @@
+window.addEventListener("dfMessengerLoaded", function (event) {
+  console.log("Chatbot cargado e inicializado correctamente" + event);
+
+  const dfMessenger = document.querySelector("df-messenger");
+  dfMessenger.renderCustomText("¡Hola! Soy SisBot, ¿En que puedo ayudarte?");
+});
+
+const dfMessenger = document.querySelector('df-messenger');
+dfMessenger.addEventListener('event-type', function (event) {
+  // Handle event
+});
+
+
+
+// function mostrarBotonSoloUnaVez() {
+
+//   var contexto = {
+//     name: "contextoSap",
+//     lifespanCount: 1,
+//     parameters: {
+//       // Aquí puedes agregar cualquier parámetro que quieras asociar con este contexto
+//     }git s
+//   };
+
+//   const contexto = "contextoSap"; 
+//   const boton = document.querySelector("button[data-name='button_sap']");
+
+
+//   if (!sessionStorage.getItem(contexto)) {
+//     boton.style.display = "block";
+//     sessionStorage.setItem(contexto, true);
+//   } else {
+//     boton.style.display = "none";
+//   }
+
+//   if (typeof codigoPersonalizado === "string" && codigoPersonalizado.trim() !== "") {
+//     eval(codigoPersonalizado);
+//     miCodigoPersonalizado();
+//   }
+// }
